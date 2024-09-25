545358871de5469dceb38a6bad062f5e0d6cb247
QuentinDerruau <78548282+QuentinDerruau@users.noreply.github.com>
2024-09-25 13:59:13 +0200

    Update chrono.bat

--- chrono.bat
+++ chrono.bat
@@ -5,10 +5,14 @@ setlocal enabledelayedexpansion
 set "seconds=0"
 
 :chrono
-cls  :: Efface l'écran de la console pour afficher un nouveau chronomètre à chaque seconde
-echo Chronometre : !seconds! seconde(s) ecoulees  :: Affiche le nombre de secondes écoulées
 
-set /a seconds+=1  :: Incrémente le compteur de secondes de 1
-timeout /t 1 >nul  :: Attend 1 seconde sans afficher de message
-
-goto chrono  :: Retourne au label :chrono pour continuer le chronomètre
+:: Efface l'écran de la console pour afficher un nouveau chronomètre à chaque seconde
+cls
+:: Affiche le nombre de secondes écoulées
+echo Chronometre : !seconds! seconde(s) ecoulees  
+:: Incrémente le compteur de secondes de 1
+set /a seconds+=1
+:: Attend 1 seconde sans afficher de message
+timeout /t 1 >nul  
+:: Retourne au label :chrono pour continuer le chronomètre
+goto chrono  
